c64e38782c32d318d253adc4221d33b7ca0740d4
Kaspi <kaspi@MacBook-Pro-Kaspi.local>
2024-06-16 17:57:05 +0500

    fix: remove proxy specification

--- src/test/java/BaseApiTest.java
+++ src/test/java/BaseApiTest.java
@@ -1,6 +1,5 @@
 import io.qameta.allure.Step;
 import io.restassured.RestAssured;
-import io.restassured.specification.ProxySpecification;
 import org.junit.Before;
 
 public class BaseApiTest {
@@ -8,12 +7,5 @@ public class BaseApiTest {
     @Step("Opening the web site")
     public void setUp() {
         RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru";
-
-//        ProxySpecification proxy = ProxySpecification.auth("zhiyenbetov_51826", "Aza_0524")
-//                .withScheme("http")
-//                .withHost("proxy-all.hq.bc")
-//                .withPort(8080);
-//
-//        RestAssured.proxy(proxy);
     }
 }
